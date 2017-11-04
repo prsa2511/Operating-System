1df192b68a7cb547d1383fb474ae3acc97afe7b6
prsa2511 <33290724+prsa2511@users.noreply.github.com>
2017-11-04 18:22:12 +0530

    Update FCFS.C

--- FCFS.C
+++ FCFS.C
@@ -1,8 +1,15 @@
 #include<conio.h>
 #include<stdio.h>
+void exchange(int *a, int *b)
+{
+ int *t;
+  *t=*a;
+  *a=*b;
+  *b=*t;
+}
 void main()
 {
-    int i,ct,p[50][50],n;
+    int i,j,ct,p[50][50],n;
     clrscr();
     printf("Enter the no. of processes\n");
     scanf("%d",&n);
@@ -14,7 +21,19 @@ void main()
     }
     printf("Entered processes are:\nPid\tAT\tBT\n");
     for (i=1;i<=n; i++)
-	printf("%d\t%d\t%d\n",p[i][0],p[i][1],p[i][2] );
+	   printf("%d\t%d\t%d\n",p[i][0],p[i][1],p[i][2] );
+     for(i=1;i<n;i++)
+     {
+     for(j=i;j<=n;j++)
+     {
+      if(p[i][1]>p[j][1])
+      {
+	exchange(&p[i][0],&p[j][0]);
+	exchange(&p[i][1],&p[j][1]);
+	exchange(&p[i][2],&p[j][2]);
+      }
+      }
+    }
     ct=0;
     for(i=1;i<=n;i++)
     {
@@ -22,11 +41,17 @@ void main()
       p[i][3]=ct;
     }
     for(i=1;i<=n;i++)
+    {
       p[i][4]=p[i][3]-p[i][1];//TAT=CT-AT
+    }
     for(i=1;i<=n;i++)
+    {
       p[i][5]=p[i][4]-p[i][2];//WT=TAT-BT
+    }
     printf("The scheduled processes are:\nPid\tAT\tBT\tCT\tTAT\tWT\n");
     for(i=1;i<=n;i++)
+    {
       printf("%d\t%d\t%d\t%d\t%d\t%d\n",p[i][0],p[i][1],p[i][2],p[i][3],p[i][4],p[i][5]);
+    }
     getch();
 }
